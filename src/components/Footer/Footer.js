9cd6ba5201e30378a0352bb7ab3a8ca5ed5306c3
Robert Petersen <robert.petersen808@gmail.com>
2021-05-30 13:45:18 -0500

    created footer files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
